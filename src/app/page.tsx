91fe344af1498c57e81e79481e038e53ddeb8b36
gustavo.andrade <gustavo.andrade@tray.net.br>
2025-03-06 20:41:30 -0300

    home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
+import PhillPointing from "@/assets/phill/v4.png"
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      Bem vindo ao Experts!
+    <div className="flex justify-center min-h-screen">
+      <div className="w-full relative">
+        <img
+          src={PhillPointing.src}
+          alt="Phill"
+          className="w-full absolute"
+          style={{ top: '10vw', left: '3vw' }}
+        />
+      </div>
+      <div
+        className="flex flex-col items-center justify-center w-full"
+        style={{ height: '80vh' }}
+      >
+        <div className="flex flex-col items-center justify-center w-full">
+          <h2 style={{ fontSize: '16vw', marginBottom: '-5vw' }}>Phill</h2>
+          <h3 style={{ fontSize: '2.4vw' }}>Seu assistente de investimentos</h3>
+        </div>
+        <div>
+          
+        </div>
+      </div>
     </div>
   );
 }
