c35b32dff74e07d23c699d219b3b7a59633590c9
gustavo.andrade <gustavo.andrade@tray.net.br>
2025-03-06 21:08:43 -0300

    cores pincipais e botoes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,11 +16,29 @@ export default function Home() {
         style={{ height: '80vh' }}
       >
         <div className="flex flex-col items-center justify-center w-full">
-          <h2 style={{ fontSize: '16vw', marginBottom: '-5vw' }}>Phill</h2>
-          <h3 style={{ fontSize: '2.4vw' }}>Seu assistente de investimentos</h3>
+          <h2 style={{ fontSize: '300px', marginBottom: '-110px' }}>Phill</h2>
+          <h3 style={{ fontSize: '44px' }}>Seu assistente de investimentos</h3>
         </div>
-        <div>
-          
+        <div className="flex flex-col items-center justify-center w-full ">
+          <button
+            className="rounded-full text-white py-2 px-6 transform transition-transform duration-300 hover:scale-110"
+            style={{
+              backgroundColor: 'var(--primary-color)',
+              margin: '80px 0px 16px 0px',
+              fontSize: '18px',
+            }}
+          >
+            Descubra seu perfil de investidor
+          </button>
+          <button
+            className="transform transition-transform duration-300 hover:scale-110"
+            style={{
+              color: 'var(--primary-color)',
+              fontSize: '18px',
+            }}
+          >
+            Ir direto ao chat
+          </button>
         </div>
       </div>
     </div>
